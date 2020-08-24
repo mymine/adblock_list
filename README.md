e10566663ea454d24fb4fcb258b8f49dde631362
uniartisan <lizhiyuan@uniartisan.com>
2020-08-24 17:46:47 +0800

    新增：隐私保护列表

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
 # adblock_list
 **一个订阅，拦截所有**
 ## 说明
-这是针对中文区的广告过滤列表，实现了精确的广告屏蔽。
+
+这是针对中文区的广告过滤列表，实现了精准的广告屏蔽。
+
 我们的原则是：**减少误杀**
+
 ### 订阅
 | 列表 	|  raw 	| jsDelivr	| Gitee	|
 | --------------------------------	|:------------------:	| ----------------	|---------------------------------------------	|
 | `adblock-basic` 	| [link](https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock.txt) 	| [link](https://cdn.jsdelivr.net/gh/uniartisan/adblock_list/adblock.txt) 	| [link](https://gitee.com/uniartisan2018/adblock_list/raw/master/adblock.txt)  	|
 | `adblock-plus` 	| [link](https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_plus.txt) 	| [link](https://cdn.jsdelivr.net/gh/uniartisan/adblock_list/adblock_plus.txt)	| [link](https://gitee.com/uniartisan2018/adblock_list/raw/master/adblock_plus.txt)	|
+| `adblock-privacy` 	| [link](https://raw.githubusercontent.com/uniartisan/adblock_list/master/adblock_privacy.txt) 	| [link](https://cdn.jsdelivr.net/gh/uniartisan/adblock_list/adblock_privacy.txt)	| [link](https://gitee.com/uniartisan2018/adblock_list/raw/master/adblock_privacy.txt)	|
 
 - **Adblock-basic** - 快速、极致的去广告体验
 
 - **Adblock-plus** - 广告，当然一个也不要放过
-当然我们也提供含有英文补充列表的 ^-^
+
+- **Adblock-privacy** - 你的隐私，在你的手里
+
 ## 规则来源
 **上游规则和自己的补充规则**
 ### 感谢
@@ -24,10 +30,17 @@
 ## 参与贡献
 本项目分为上游列表和补充列表，如果您想要参与我们，您可以Fork、Pull request。
 1. **如何参与贡献？**
+
 项目的dev文件夹中分别有黑名单和白名单，您可以修改他们并提交，提交要注明广告的复现流程。
+
 2. **上游列表有误**
-反馈英文网站或EasyList或EasyPrivacy或Fanboy's Social Blocking List的问题：请将网址发到easylist.subscription@gmail.com并英文描述
+
+反馈英文网站或EasyList或EasyPrivacy或Fanboy's Social Blocking List的问题：请将网址发到easylist.
+subscription@gmail.com并英文描述
+
 反馈非中英文网站的问题：请查看对应的语言列表https://adblockplus.org/zh_CN/subscriptions
+
 3. **如何提问？**
 [提问的艺术（中文版）](https://github.com/ryanhanwu/How-To-Ask-Questions-The-Smart-Way/blob/master/README-zh_CN.md)
+
 **请注意，提供此翻译的个人及组织不对本项目负责**
--- scripts/tools/adblock.py
+++ scripts/tools/adblock.py
@@ -14,22 +14,27 @@ os.system('python3 %s' % downpydir)
 
 rawdata = []
 rawdata_plus = []
+rawdata_privacy = []
 with open(os.path.join(libdir, 'metadata.txt'), 'r', encoding='UTF-8') as f:
     for line in f:
         temp = line.strip('\n')
         if temp.startswith('!') or temp.startswith('[') or temp.startswith('#') or len(temp) == 0:
             continue
         else:
-            if not temp.startswith('?'):
-                rawdata.append(temp)
-            else:
+            if temp.startswith('?'):
                 rawdata_plus.append(temp.strip('?'))
+            elif temp.startswith('&'):
+                rawdata_privacy.append(temp.strip('&'))
+            else:
+                rawdata.append(temp)
 
 
 result = []
 result_plus = []
-data_lenth = len(rawdata)
+result_privacy = []
 
+# Baisc 处理
+data_lenth = len(rawdata)
 for t in range(0, data_lenth):
     with open(os.path.join(rawdir, rawdata[t]), 'r', encoding='UTF-8') as f:
         for line in f:
@@ -41,7 +46,6 @@ for t in range(0, data_lenth):
                 result_plus.append(temp)
 
 # Plus 处理
-
 data_lenth = len(rawdata_plus)
 for t in range(0, data_lenth):
     with open(os.path.join(rawdir, rawdata_plus[t]), 'r', encoding='UTF-8') as f:
@@ -52,11 +56,24 @@ for t in range(0, data_lenth):
             else:
                 result_plus.append(temp)
 
+# Privacy 处理
+data_lenth = len(rawdata_privacy)
+for t in range (0, data_lenth):
+    with open(os.path.join(rawdir, rawdata_privacy[t]), 'r', encoding='UTF-8') as f:
+        for line in f:
+            temp = line.strip('\n')
+            if temp.startswith('!') or temp.startswith('[')  or len(temp) == 0:
+                continue
+            else:
+                result_privacy.append(temp)
+
 # 排序去重
 result = list(set(result))
 result.sort()
 result_plus = list(set(result_plus))
 result_plus.sort()
+result_privacy = list(set(result_privacy))
+result_privacy.sort()
 
 # 时间戳信息
 time_now = datetime.datetime.now()
@@ -83,3 +100,13 @@ with open(os.path.join(outputdir, "adblock_plus.txt"), "w", encoding='UTF-8') as
             str(lenth_plus)+'\n')
     for i in range(1, lenth_plus):
         f.write(result_plus[i]+'\n')
+
+with open(os.path.join(outputdir, "adblock_privacy.txt"), "w", encoding='UTF-8') as f:
+    f.write('[uniartisan\'s Privacy List]\n'+'! Version: ' +
+            date_string+'\n'+'! Title:  uniartisan\'s Privacy List\n' +
+            '! Expires: 1 days (update frequency)\n')
+    lenth_privay = len(result_privacy)
+    f.write('! URL = https://github.com/uniartisan/adblock_list\n! Lenth = ' +
+            str(lenth_privay)+'\n')
+    for i in range(1, lenth_privay):
+        f.write(result_privacy[i]+'\n')
